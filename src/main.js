ee0926d438614732dfe2fdaee5f5ea5c7ce6144a
admin <zhaobing966@163.com>
2019-04-02 12:39:21 +0800

    init my project

---
+++ src/main.js
@@ -0,0 +1,19 @@
+// 入口文件
+import Vue from 'vue'
+
+// 导入MUI的样式
+import './lib/mui/css/mui.min.css'
+
+// 按需导入 Mint-Ui 中的组件
+import { Header } from 'mint-ui'
+Vue.component(Header.name, Header)
+
+
+// 导入 App 根组件
+import app from './App.vue'
+import { from } from 'array-flatten';
+
+var vm = new Vue({
+    el: '#app',
+    render: c => c(app)
+})
