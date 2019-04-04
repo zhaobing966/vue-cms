6f092437b7fdc3864c7fb4b134884b0546235315
admin <zhaobing966@163.com>
2019-04-04 15:15:51 +0800

    项目第1天打卡

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # 这是一个 vue-cms 项目
 # 哈哈哈
+
+## 用（传统方式）命令行把修改过后的代码上传到GitHub
+1. git add .
+2. git commit -m "提交信息"
+3. git push
+
+## 制作首页App组件
+1. 完成 Header区域，使用的是 Mint-UI 中的Header组件
+2. 制作底部的 Tabbar 区域，使用的是 MUI 的 Tabbar.html
+3. 要在中间区域放置一个 router-view 来展示路由匹配到的组件
--- src/main.js
+++ src/main.js
@@ -1,12 +1,30 @@
 // 入口文件
 import Vue from 'vue'
+// 1.1导入路由的包
+import VueRouter from 'vue-router'
+// 1.2安装路由
+Vue.use(VueRouter)
+
+// 2.1 导入 vue-resource
+import VueResource from 'vue-resource'
+
+// 2.2 安装 vue-resource
+Vue.use(VueResource)
 
 // 导入MUI的样式
 import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
+
 
 // 按需导入 Mint-Ui 中的组件    
-import { Header } from 'mint-ui'
+import { Header, Swipe, SwipeItem } from 'mint-ui'
 Vue.component(Header.name, Header)
+Vue.component(Swipe.name, Swipe)
+Vue.component(SwipeItem.name, SwipeItem)
+
+
+// 1.3导入自己的 router.js 路由模块
+import router from './router.js'
 
 
 // 导入 App 根组件
@@ -15,5 +33,6 @@ import { from } from 'array-flatten';
 
 var vm = new Vue({
     el: '#app',
-    render: c => c(app)
+    render: c => c(app),
+    router //挂载路由对象到 vm 实例上
 })
